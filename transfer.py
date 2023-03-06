c92a19407c30ab04351c7cd55259463c699b6dfa
Pogajoa <87626122+Pogajoa@users.noreply.github.com>
2023-03-06 19:47:31 +0900

    Add files via upload

--- transfer.py
+++ transfer.py
@@ -6,4 +6,4 @@ prov_list.sort(key=lambda x:int(x.split('.')[0]))
 #print(prov_list)
 for name in prov_list:
     img1 = cv2.imread("./youtube_3d_hands/data/youtube/G26Ly2CTUy0/video/frames/" + name)
-    cv2.imwrite('./concatenate/images/original/' + name, img1)    
+    cv2.imwrite('./concatenate/images/original/' + name, img1)
